30a7c6288b02c6060d122e0540100726c8326ba6
Jack Braun <j.braun3456@gmail.com>
2025-05-29 00:18:08 -0400

    finished test flowControl and equality

--- src/6.flowControl.js
+++ src/6.flowControl.js
@@ -7,7 +7,32 @@
  *    if no number was provided or if the value provided wasn't a number (hint: typeof), return false
  */
 function fizzBuzz(num) {
-  
+
+if (typeof num !== 'number')
+  {
+    return false;
+  }
+
+  if ((num % 5 == 0) && (num % 3 == 0))
+  {
+    return 'fizzbuzz';
+  }
+
+  if (num % 3 == 0)
+  {
+    return 'fizz';
+  }
+
+  if (num % 5 == 0)
+  {
+    return 'buzz';
+  }
+
+  if ((num % 5 !== 0) && (num % 3 !== 0))
+  {
+    return num;
+  }
+
 }
 
 module.exports = {
